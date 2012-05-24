6e695575b32a6e31ef1c5748a34f1cfcf024aad6
MrSnukke <MrSnukke@emaillime.com>
2012-05-24 12:12:01 +0200

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base 
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -28,6 +29,10 @@ class User < ActiveRecord::Base
   
   validates :password_confirmation, presence: true
   
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
   
   private
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -70,9 +70,7 @@ describe "Authentication" do
           end
         end
       end
-
 	  
-	  ##DELETE FROM HERE???
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -84,13 +82,26 @@ describe "Authentication" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
-	  ##DELETE TO HERE???
 		
 		describe "visiting the user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
 		
+      end
+	  
+	  
+	  describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
       end
 
 	  
